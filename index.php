01731e2fdb739265efd011abeeb0b6bc34fe4153
Jerome Haas <jerome.haas@arcmedia.ch>
2020-08-30 09:57:59 +0200

    Do some css fine-tuning

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 
 
 
+
     <div class="section-hero">
             <img
                     class="section-hero__image"
--- lang/de/partials/navigation_desktop.php
+++ lang/de/partials/navigation_desktop.php
@@ -1,11 +1,13 @@
 <nav class="navigation-desktop">
-    <div class="navigation-desktop__bar">
-        <a class="navigation-desktop__logo__link" href="http://localhost:8888/grafik_heller/index.php">
-            <img class="navigation-desktop__logo" src="http://localhost:8888/grafik_heller/media/logo/logo.svg" alt="heller grafik">
-        </a>
-        <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/work.php">Arbeiten</a>
-        <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/info.php">Info</a>
-        <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/contact.php">Kontakt</a>
+        <div class="navigation-desktop__bar">
+            <div class="navigation_desktop__container">
+            <a class="navigation-desktop__logo__link" href="http://localhost:8888/grafik_heller/index.php">
+                <img class="navigation-desktop__logo" src="http://localhost:8888/grafik_heller/media/logo/logo.svg" alt="heller grafik">
+            </a>
+            <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/work.php">Arbeiten</a>
+            <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/info.php">Info</a>
+            <a class="navigation-desktop__navigation-item link-navigation default-link" href="http://localhost:8888/grafik_heller/lang/de/pages/contact.php">Kontakt</a>
+        </div>
     </div>
     <div class="navigation-desktop__placeholder"></div>
 </nav>
