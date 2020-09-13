29f94c8fda355259d911a4a6e22b0642e1b1f7d4
Jérôme Haas <j.haas@cyberplanet.ch>
2020-09-13 13:36:22 +0200

    Add privacy.php

---
+++ lang/de/pages/privacy.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <?php include '../partials/head.php' ?>
+    <title>Impressum</title>
+    <meta name="description" content="Copyright © 2020 Isabel Heller. Alle Rechte vorbehalten. Für die Vervielfältigung oder Veränderung jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.">
+</head>
+<body>
+
+
+<!-- NAVIGATION DESKTOP -->
+<?php include '../partials/navigation_desktop.php' ?>
+<!-- NAVIGATION DESKTOP -->
+
+<!-- NAVIGATION MOBILE -->
+<?php include '../partials/navigation_mobile.php' ?>
+<!-- NAVIGATION MOBILE -->
+
+<div id="page-wrapper">
+
+    <main class="section-privacy">
+        <h3 class="privacy__title">Datenschutzerklärung</h3>
+        <div class="privacy__container">
+            <div class="privacy__card">
+                <p class="privacy__card__text">Diese Datenschutzerklärung beinhaltet allgemeinen Angaben zum Umgang von heller grafik mit Ihren Daten sowie Informationen über Ihre Rechte gemäss der Europäischen Datenschutz-Grundverordnung (DSGVO) und dem Bundesdatenschutzgesetz (BDSG).</p>
+            </div>
+            <address class="privacy__card">
+                <p class="privacy__card__title">Name und Anschrift des für die Datenverarbeitung Verantwortlichen</p>
+                <p class="privacy__card__text">Isabel Heller</p>
+                <p class="privacy__card__text">Zähringerstrasse 13</p>
+                <p class="privacy__card__text">6003 Luzern</p>
+            </address>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Datenschutzgesetz</p>
+                <p class="privacy__card__text">Gestützt auf Artikel 13 der schweizerischen Bundesverfassung und die datenschutz-rechtlichen Bestimmungen des Bundes (Datenschutzgesetz, DSG) hat jede Person Anspruch auf Schutz ihrer Privatsphäre sowie auf Schutz vor Missbrauch ihrer persönlichen Daten. Wir halten diese Bestimmungen ein. Persönliche Daten werden streng vertraulich behandelt und weder an Dritte verkauft noch weiter gegeben. In enger Zusammenarbeit mit unseren Hosting-Providern bemühen wir uns, die Datenbanken so gut wie möglich vor fremden Zugriffen, Verlusten, Missbrauch oder vor Fälschung zu schützen. Beim Zugriff auf unsere Webseiten werden folgende Daten in Logfiles gespeichert: IP-Adresse, Datum, Uhrzeit, Browser-Anfrage und allg. übertragene Informationen zum Betriebssystem resp. Browser. Diese Nutzungsdaten bilden die Basis für statistische, anonyme Auswertungen, so dass Trends erkennbar sind, anhand derer wir unsere Angebote entsprechend verbessern können.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Allgemeines zur Datenverarbeitung</p>
+                <p class="privacy__card__text">Der Schutz Ihrer Daten ist uns wichtig. Wir respektieren Ihre Privatsphäre und setzen uns dafür ein, dass Ihre Daten gemäß den geltenden Gesetzen bearbeitet werden. Wir verarbeiten personenbezogene Daten grundsätzlich nur, soweit dies zur Bereitstellung einer funktionsfähigen Website sowie unserer Inhalte und Leistungen erforderlich ist. Die Verarbeitung personenbezogener Daten der betroffenen Person erfolgt nur nach und mit Ihrer Einwilligung. Eine Ausnahme gilt in solchen Fällen, in denen eine vorherige Einholung einer Einwilligung aus tatsächlichen Gründen nicht möglich ist und die Verarbeitung der Daten durch gesetzliche Vorschriften gestattet ist. Mit der vorliegenden Datenschutzerklärung informieren wir Sie umfassend über unsere Datenbearbeitungen.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Bearbeitung von Personaldaten</p>
+                <p class="privacy__card__text">Unter Bearbeitung von Personendaten fallen z.B. die</p>
+                <ul class="privacy__card__list">
+                    <li class="privacy__card__list-element">Erhebung</li>
+                    <li class="privacy__card__list-element">Speicherung</li>
+                    <li class="privacy__card__list-element">Nutzung</li>
+                    <li class="privacy__card__list-element">Übermittlung</li>
+                    <li class="privacy__card__list-element">Bekanntgabe</li>
+                    <li class="privacy__card__list-element">Löschung</li>
+                </ul>
+                <p class="privacy__card__text">von Personendaten.</p>
+                <br />
+                <p class="privacy__card__text"><p class="privacy__card__text">Wir sammeln Personendaten, welche bei Ihrem Besuch auf unserer Website automatisch erfasst werden. Es sind dies z.B. die Aktivitäten auf der heller grafik Webseite und die IP-Adresse von technischen Geräten..</p></p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Verwendung der Personaldaten</p>
+                <p class="privacy__card__text">Wir verwenden Ihre Personendaten in erster Linie zum Zweck, um Daten über unsere Zielgruppe zu sammeln, damit wir die Webseite bestmöglich auf deren Bedürfnisse abstimmen können</p>
+            </div>
+            <div class="privacy__card privacy__realisation">
+                <p class="privacy__card__title">An wen gehen die Personendaten weiter?</p>
+                <p class="privacy__card__text">Wir leiten Ihre Daten, falls für die hier in der Datenschutzerklärung genannten Zwecke notwendig, an Dritte weiter, um z.B. technische oder organisatorische Dienstleistungen in Anspruch zu nehmen, die wir für die Erfüllung der genannten Zwecke oder unserer sonstigen Geschäftstätigkeit benötigen. Unsere Dienstleistenden sind verpflichtet, die Personendaten ausschließlich in unserem Auftrag und nach unseren Instruktionen zu bearbeiten. Wir verpflichten unsere Dienstleistenden zur Einhaltung von technischen und organisatorischen Maßnahmen, welche den Schutz der Personendaten sicherstellen.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Dauer der Aufbewahrung von Personendaten</p>
+                <p class="privacy__card__text">Die personenbezogenen Daten der betroffenen Person werden gelöscht oder gesperrt, sobald der Zweck der Speicherung entfällt.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Bereitstellung der Webseite und Erstellung von Logfiles</p>
+                <br />
+                <p class="privacy__card__title">Protokollierung</p>
+                <p class="privacy__card__text">Bei jedem Aufruf unserer Internetseite erfasst unser System automatisiert Daten und Informationen vom Computersystem des aufrufenden Rechners. Bei jedem Zugriff auf die Seiten von heller grafik werden Nutzungsdaten durch den jeweiligen Internetbrowser übermittelt und in Protokolldateien, den sogenannten Server-Logfiles, gespeichert. Die dabei gespeicherten Datensätze enthalten die folgenden Daten: Datum und Uhrzeit des Abrufs, Name der aufgerufenen Seite, IP-Adresse, Referrer-URL (Herkunfts-URL, von der aus Sie auf die Webseiten gekommen sind), die übertragene Datenmenge, sowie Produkt und Versions-Informationen des verwendeten Browsers. Diese Logfile-Datensätze werten wir aus, um unser Angebot auf der heller grafik Website weiter zu verbessern und nutzerfreundlicher zu gestalten, Fehler schneller zu finden und zu beheben sowie Serverkapazitäten zu steuern. Zugleich nutzen wir die Logfile-Datensätze zur Abwehr von Angriffen auf unsere heller grafik Website z.B. im Rahmen von sogenannten Distributed-Denial-of- Service-Attacken, die darauf abzielen, den Zugriff auf unseren heller grafik Website durch eine Überlastung mit Anfragen zu blockieren. Welche Daten genau erhoben werden, ist in den Abschnitten zu den einzelnen Analysetools aufgeführt.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Zweck und Dauer der Datenverarbeitung</p>
+                <p class="privacy__card__text">Die vorübergehende Speicherung der IP-Adresse durch das System ist notwendig, um eine Auslieferung der Website an den Rechner des Nutzers / der Nutzerin zu ermöglichen. Hierfür muss die IP-Adresse des Nutzers / der Nutzerin für die Dauer der Sitzung gespeichert bleiben. Die Daten werden gelöscht, sobald sie für die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Widerspruchs- und Beseitigungsmöglichkeit</p>
+                <p class="privacy__card__text">Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles ist für den Betrieb der Internetseite zwingend erforderlich. Es besteht folglich seitens des Nutzers / der Nutzerin keine Widerspruchsmöglichkeit.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Cookies</p>
+                <p class="privacy__card__text">Die Internetseiten verwenden teilweise so genannte Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf Ihrem Rechner abgelegt werden und die Ihr Browser speichert. Die meisten der von uns verwendeten Cookies sind so genannte «Session-Cookies». Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen. Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschließen sowie das automatische Löschen der Cookies beim Schliessen des Browsers aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein. Cookies, die zur Durchführung des elektronischen Kommunikationsvorgangs oder zur Bereitstellung bestimmter, von Ihnen erwünschter Funktionen z.B. Warenkorbfunktion) erforderlich sind, werden gespeichert. Der Websitebetreiber hat ein berechtigtes Interesse an der Speicherung von Cookies zur technisch fehlerfreien und optimierten Bereitstellung seiner Dienste. Soweit andere Cookies (z.B. Cookies zur Analyse Ihres Surfverhaltens) gespeichert werden, werden diese in dieser Datenschutzerklärung gesondert behandelt.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Datenschutzerklärung für die Nutzung von Instagram</p>
+                <p class="privacy__card__text">Auf unserer Website werden auch sogenannte Social Plugins («Plugins») von Instagram verwendet, das von der Instagram LLC., 1601 Willow Road, Menlo Park, CA 94025, USA (Instagram) betrieben wird. Die Plugins sind mit einem Link-Button «Instagram» gekennzeichnet. Wenn Sie eine Seite unseres Webauftritts aufrufen, die ein solches Plugin enthält, stellt Ihr Browser eine direkte Verbindung zu den Servern von Instagram her. Der Inhalt des Plugins wird von Instagram direkt an Ihren Browser übermittelt und in die Seite eingebunden. Durch diese Einbindung erhält Instagram die Information, dass Ihr Browser die entsprechende Seite unseres Webauftritts aufgerufen hat, auch wenn Sie kein Instagram-Profil besitzen oder gerade nicht bei Instagram eingeloggt sind. Diese Information (einschließlich Ihrer IP-Adresse) wird von Ihrem Browser direkt an einen Server von Instagram in die USA übermittelt und dort gespeichert. Sind Sie bei Instagram eingeloggt, kann Instagram den Besuch unserer Website Ihrem Instagram-Account unmittelbar zuordnen. Wenn Sie mit den Plugins interagieren, zum Beispiel den Instagram-Button betätigen, wird diese Information ebenfalls direkt an einen Server von Instagram übermittelt und dort gespeichert. Die Informationen werden außerdem auf Ihrem Instagram-Account veröffentlicht und dort Ihren Kontakten angezeigt. Wenn Sie nicht möchten, dass Instagram die über unseren Webauftritt gesammelten Daten unmittelbar Ihrem Instagram-Account zuordnet, müssen Sie sich vor Ihrem Besuch unserer Website bei Instagram ausloggen. Weitere Informationen hierzu Sie in der Datenschutzerklärung (https://help.instagram.com/155833707900388) von Instagram.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Webanalyse</p>
+                <br />
+                <p class="privacy__card__title">Datenschutzerklärung für die Nutzung von Google Analytics</p>
+                <p class="privacy__card__text">Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. («Google»). Google Analytics verwendet sog. «Cookies», Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf dieser Webseite wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Haftung für Links</p>
+                <p class="privacy__card__text">heller grafik erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat heller grafik keinerlei Einfluss. Deshalb distanziert sie sich hiermit ausdrücklich von allen Inhalten aller verlinkten/verknüpften Seiten, die nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und Verweise. Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist. Aktualität und Änderung dieser Datenschutzerklärung Diese Datenschutzerklärung ist aktuell gültig und hat den Stand September 2020. Durch die Weiterentwicklung unserer Website und Angebote darüber oder aufgrund geänderter gesetzlicher beziehungsweise behördlicher Vorgaben kann es notwendig werden, diese Datenschutzerklärung zu ändern.</p>
+            </div>
+            <div class="privacy__card">
+                <p class="privacy__card__title">Aktualität und Änderung dieser Datenschutzerklärung</p>
+                <p class="privacy__card__text">Diese Datenschutzerklärung ist aktuell gültig und hat den Stand September 2020. Durch die Weiterentwicklung unserer Website und Angebote darüber oder aufgrund geänderter gesetzlicher beziehungsweise behördlicher Vorgaben kann es notwendig werden, diese Datenschutzerklärung zu ändern.</p>
+            </div>
+            <p class="privacy__copyright">
+                Copyright © 2020 Isabel Heller. Alle Rechte vorbehalten. Für die Vervielfältigung oder Veränderung jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.
+            </p>
+        </div>
+    </main>
+
+</div> <!-- END OF PAGE-WRAPPER -->
+
+<!-- FOOTER -->
+<?php include '../partials/footer_mobile.php' ?>
+<?php include '../partials/footer_desktop.php' ?>
+<!-- FOOTER -->
+
+<script src="http://localhost:8888/hellergrafik/js/jquery.js"></script>
+<script src="http://localhost:8888/hellergrafik/js/ux.js"></script>
+
+</body>
+</html>
